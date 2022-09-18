5bdc9f31542a6feec432b034ba25c85df5e9c3b2
Ivan-Tarasenko <80888936+Ivan-Tarasenko@users.noreply.github.com>
2022-09-18 19:58:31 +1100

    first commit

---
+++ Calculator_SF/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator_SF
+//
+//  Created by Иван Тарасенко on 18.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
